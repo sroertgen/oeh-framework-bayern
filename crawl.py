f8294cedb7f2c269dec4591cb484b2a3c68c6a4e
sroertg <steffen.roertgen@gwdg.de>
2020-04-29 08:16:46 +0200

    deleted unnecessary scrapy scripts, this is a bs4 job

--- crawl.py
+++ crawl.py
@@ -2,10 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 
-workbook = xlsxwriter.Workbook('curriculum_new.xlsx')
+workbook = xlsxwriter.Workbook('curriculum.xlsx')
 worksheet = workbook.add_worksheet()
 
-row_sheet = 2
 
 subjects = [
     'beruf_und_arbeit',
@@ -39,7 +38,7 @@ subjects = [
 
 educationalContext = [
     'grundschule',
-    'foerderschule'
+    'foerderschule',
     'mittelschule',
     'realschule',
     'gymnasium'
@@ -69,23 +68,84 @@ for item in sheets_columns:
     ch = chr(ord(ch) + 1)
 
 
+# global vars
+row_sheet = 2
+listEnumeration = [1, 0, 0]
+
+
 def get_item_title(soup, subject, context, grade):
     global row_sheet
-    for item in soup.find_all('span', 'head-absatz-title-short'):
-        fullStatement = item.get_text().strip()
-        humanCodingScheme = context + '_' + subject + '_' + str(grade)
-        listEnumeration = row_sheet
-        row_b = 'B' + str(row_sheet)
-        row_c = 'C' + str(row_sheet)
-        row_d = 'D' + str(row_sheet)
-        row_i = 'I' + str(row_sheet)
-        row_j = 'J' + str(row_sheet)
-        worksheet.write(row_b, fullStatement)
-        worksheet.write(row_c, humanCodingScheme)
-        worksheet.write(row_d, listEnumeration)
-        worksheet.write(row_i, 'de')
-        worksheet.write(row_j, grade)
-        row_sheet += 1
+    global listEnumeration
+    div = soup.find_all('div', 'toggable')
+    try:
+        for i, item in enumerate(div):
+            if 'headline_lvl1' in item['class']:
+                if item.find('div', 'thema_absch') != None:
+                    description = ' - ' + item.find('div', 'thema_absch').get_text(
+                    ).strip().replace('\n', ' ').replace('\r', '')
+                else:
+                    description = ''
+                fullStatement = item.find(
+                    'span', 'head-absatz-title-short').get_text().strip() + description
+                fach_jgs = item.find(
+                    'span', 'headline_fach_jgs').get_text().strip()
+                humanCodingScheme = fach_jgs + '_' + subject + '_' + \
+                    str(grade) + '_' + str(listEnumeration[0])
+                row_b = 'B' + str(row_sheet)
+                row_c = 'C' + str(row_sheet)
+                row_d = 'D' + str(row_sheet)
+                row_i = 'I' + str(row_sheet)
+                row_j = 'J' + str(row_sheet)
+                worksheet.write(row_b, fullStatement)
+                worksheet.write(row_c, humanCodingScheme)
+                worksheet.write(row_d, listEnumeration[0])
+                worksheet.write(row_i, 'de')
+                worksheet.write(row_j, grade)
+                row_sheet += 1
+
+                if 'headline_lvl2' in div[i+1]['class']:
+                    listEnumeration[1] = listEnumeration[1] + 1
+                else:
+                    listEnumeration[0] = listEnumeration[0] + 1
+
+            if 'headline_lvl2' in item['class']:
+                if item.find('div', 'thema_absch') != None:
+                    description = ' - ' + item.find('div', 'thema_absch').get_text(
+                    ).strip().replace('\n', ' ').replace('\r', '')
+                else:
+                    description = ''
+                fullStatement = item.find(
+                    'span', 'head-absatz-title-long').get_text().strip() + description
+                fach_jgs = item.find(
+                    'span', 'headline_fach_jgs').get_text().strip()
+                smartLevel = str(
+                    listEnumeration[0]) + '.' + str(listEnumeration[1])
+                humanCodingScheme = fach_jgs + '_' + subject + '_' + \
+                    str(grade) + '_' + smartLevel
+                row_b = 'B' + str(row_sheet)
+                row_c = 'C' + str(row_sheet)
+                row_d = 'D' + str(row_sheet)
+                row_i = 'I' + str(row_sheet)
+                row_j = 'J' + str(row_sheet)
+                worksheet.write(row_b, fullStatement)
+                worksheet.write(row_c, humanCodingScheme)
+                worksheet.write(row_d, smartLevel)
+                worksheet.write(row_i, 'de')
+                worksheet.write(row_j, grade)
+                row_sheet += 1
+
+                if 'headline_lvl2' in div[i+1]['class']:
+                    listEnumeration[1] = listEnumeration[1] + 1
+                elif 'headline_lvl1' in div[i+1]['class']:
+                    listEnumeration[0] = listEnumeration[0] + 1
+                    listEnumeration[1] = 0
+
+        listEnumeration[0] = listEnumeration[0] + 1
+        listEnumeration[1] = 0
+    except IndexError:
+        listEnumeration[0] = listEnumeration[0] + 1
+        listEnumeration[1] = 0
+        pass
 
 
 for grade in range(1, 14):
